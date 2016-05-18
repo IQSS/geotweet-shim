05a462b2988d6886136d187e741520a74a4b2e34
Raman Prasad <raman_prasad@harvard.edu>
2016-05-18 06:41:33 -0400

    some initial files..

---
+++ interactive_dataset/geotweets/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+
+    d = dict(title="A Billion Streaming GeoTweets",
+            contributor="Harvard Center for Geographic Analysis")
+    return render(request, 'index.html', d)
+
+
+def open_layers_test(request):
+
+    d = dict(title="A Billion Streaming GeoTweets",
+            contributor="Harvard Center for Geographic Analysis")
+    return render(request, 'ol_test_01.html', d)
