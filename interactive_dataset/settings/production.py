a68f3f20c9ebe5079f77d6d067dadd734d4f7c56
Raman Prasad <raman_prasad@harvard.edu>
2016-06-08 17:14:32 -0400

    remove email attrs for now

--- interactive_dataset/settings/production.py
+++ interactive_dataset/settings/production.py
@@ -12,9 +12,11 @@ DATABASES['default'].update(db_from_env)
 
 ## Email settings in Herok Config Vars
 
+"""
 EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_PORT = os.environ['EMAIL_PORT']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
 EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+"""
